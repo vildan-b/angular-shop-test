4fbf418806254c688b34b5b3c51d5ba199d1367e
vildan-b <vildanb@gmx.de>
2022-03-23 11:30:52 +0100

    Category-Component“

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+  title = "Category List";
+  categories : Category[] =[
+    {id:1, name:"Elektronik"},
+    {id:2, name:"Computer"},
+    {id:3, name:"Film"},
+    {id:4, name:"Music"},
+    {id:5, name:"Communication"},
 
+  ]
   ngOnInit(): void {
   }
 
