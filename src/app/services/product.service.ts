c2046ca60fbfd71bdcead347b15fad8fd8bf2f43
vildan-b <vildanb@gmx.de>
2022-03-24 15:21:15 +0100

    logging with observable

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { Observable, throwError, catchError } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable()
@@ -9,7 +10,24 @@ export class ProductService {
   path = "http://localhost:3000/products";
 
   constructor(private http: HttpClient) { }
-  getProducts():Observable<Product[]>{
-    return this.http.get<Product[]>(this.path)
-    };
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.path).pipe(
+      tap((data) => console.log(JSON.stringify(data) )),
+      catchError(this.handleError)
+    );
   }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage= ''
+
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'something went wrong ' + err.error.message
+    
+    }else{
+      errorMessage = 'aaaaaaaaa'
+    } 
+    return throwError(errorMessage);
+    }
+  }
+
+  
+  
